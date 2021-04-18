6786c057fa690cd7f26603d00096c2c7285353b9
bramantyaadi <bramantyaadi44@gmail.com>
2021-04-18 17:27:59 +0700

    add table h_jual , d_jual , h_beli , d_beli

--- Adamart/database/pos_sep.sql
+++ Adamart/database/pos_sep.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100411
  File Encoding         : 65001
 
- Date: 17/04/2021 20:44:16
+ Date: 18/04/2021 17:26:22
 */
 
 SET NAMES utf8mb4;
@@ -34,6 +34,85 @@ CREATE TABLE `barang`  (
 
 -- ----------------------------
 -- Records of barang
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for d_beli
+-- ----------------------------
+DROP TABLE IF EXISTS `d_beli`;
+CREATE TABLE `d_beli`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_h_beli` int(11) NULL DEFAULT NULL,
+  `id_barang` int(11) NULL DEFAULT NULL,
+  `qty` int(11) NULL DEFAULT NULL,
+  `sub_total` int(11) NULL DEFAULT NULL,
+  `created_at` date NULL DEFAULT NULL,
+  `updated_at` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of d_beli
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for d_jual
+-- ----------------------------
+DROP TABLE IF EXISTS `d_jual`;
+CREATE TABLE `d_jual`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_barang` int(11) NULL DEFAULT NULL,
+  `id_h_jual` int(11) NULL DEFAULT NULL,
+  `qty` int(11) NULL DEFAULT NULL,
+  `sub_total` int(11) NULL DEFAULT NULL,
+  `created_at` date NULL DEFAULT NULL,
+  `updated_at` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of d_jual
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for h_beli
+-- ----------------------------
+DROP TABLE IF EXISTS `h_beli`;
+CREATE TABLE `h_beli`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_supplier` int(11) NULL DEFAULT NULL,
+  `nota_pembelian` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `created_at` date NULL DEFAULT NULL,
+  `updated_at` date NULL DEFAULT NULL,
+  `deleted_at` int(11) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of h_beli
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for h_jual
+-- ----------------------------
+DROP TABLE IF EXISTS `h_jual`;
+CREATE TABLE `h_jual`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nota_jual` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `id_member` int(11) NULL DEFAULT NULL,
+  `id_pegawai` int(11) NULL DEFAULT NULL,
+  `tanggal` date NULL DEFAULT NULL,
+  `total` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `bayar` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `kembali` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `diskon` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `created_at` date NULL DEFAULT NULL,
+  `updated_at` date NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of h_jual
 -- ----------------------------
 
 -- ----------------------------
@@ -70,24 +149,6 @@ CREATE TABLE `member`  (
 
 -- ----------------------------
 -- Records of member
--- ----------------------------
-
--- ----------------------------
--- Table structure for nota_pembelian
--- ----------------------------
-DROP TABLE IF EXISTS `nota_pembelian`;
-CREATE TABLE `nota_pembelian`  (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `id_supplier` int(11) NULL DEFAULT NULL,
-  `nota_pembelian` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `created_at` date NULL DEFAULT NULL,
-  `updated_at` date NULL DEFAULT NULL,
-  `deleted_at` int(11) NULL DEFAULT NULL,
-  PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
-
--- ----------------------------
--- Records of nota_pembelian
 -- ----------------------------
 
 -- ----------------------------
