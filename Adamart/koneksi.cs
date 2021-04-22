ab0d42b0fe97b694d6237814a65abe6926cb4d93
Rolan Oktafian <rolantwin@gmail.com>
2021-04-22 19:42:37 +0700

    koneksi db online

--- Adamart/koneksi.cs
+++ Adamart/koneksi.cs
@@ -10,7 +10,7 @@ namespace Adamart
     {
         public string conn()
         {
-            return "Host=localhost;Uid=root;Pwd=;database=pos_sep;";
+            return "Host=153.92.9.15;Uid=u3657402_adamart;Pwd=adamart;database=u3657402_adamart;";
         }
     }
 }
