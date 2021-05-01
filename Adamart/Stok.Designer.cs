64efa939d07522cdf42e291ac2912b8f8ea4fd99
twinkokei <rolantwin@gmail.com>
2021-05-01 14:46:49 +0700

    stok sitik

--- Adamart/Stok.Designer.cs
+++ Adamart/Stok.Designer.cs
@@ -31,16 +31,24 @@ namespace Adamart
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Id = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nama_barang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.harga_barang = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.id_kategori = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(800, 450);
             this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
             // 
             // label1
             // 
@@ -51,6 +59,52 @@ namespace Adamart
             this.label1.Size = new System.Drawing.Size(73, 31);
             this.label1.TabIndex = 3;
             this.label1.Text = "Stok";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Id,
+            this.nama_barang,
+            this.harga_barang,
+            this.id_kategori});
+            this.dataGridView1.Location = new System.Drawing.Point(18, 44);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(770, 369);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // Id
+            // 
+            this.Id.DataPropertyName = "id";
+            this.Id.Frozen = true;
+            this.Id.HeaderText = "Id";
+            this.Id.Name = "Id";
+            this.Id.ReadOnly = true;
+            // 
+            // nama_barang
+            // 
+            this.nama_barang.DataPropertyName = "nama_barang";
+            this.nama_barang.Frozen = true;
+            this.nama_barang.HeaderText = "Nama Barang";
+            this.nama_barang.Name = "nama_barang";
+            this.nama_barang.ReadOnly = true;
+            // 
+            // harga_barang
+            // 
+            this.harga_barang.DataPropertyName = "harga_barang";
+            this.harga_barang.Frozen = true;
+            this.harga_barang.HeaderText = "Harga Barang";
+            this.harga_barang.Name = "harga_barang";
+            this.harga_barang.ReadOnly = true;
+            // 
+            // id_kategori
+            // 
+            this.id_kategori.DataPropertyName = "id_kategori";
+            this.id_kategori.Frozen = true;
+            this.id_kategori.HeaderText = "Kategori ID";
+            this.id_kategori.Name = "id_kategori";
+            this.id_kategori.ReadOnly = true;
             // 
             // Stok
             // 
@@ -64,6 +118,7 @@ namespace Adamart
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -72,5 +127,10 @@ namespace Adamart
 
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Id;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nama_barang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn harga_barang;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_kategori;
     }
 }
--- Adamart/Stok.cs
+++ Adamart/Stok.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +17,18 @@ namespace Adamart
         public Stok()
         {
             InitializeComponent();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            koneksi cone = new koneksi();
+            MySqlConnection conn = new MySqlConnection(cone.conn());
+            conn.Open();
+            DataSet ds = new DataSet();
+            MySqlDataAdapter da = new MySqlDataAdapter("SELECT id,id_kategori,nama_barang,harga_barang FROM barang", conn);
+            da.Fill(ds, "userop");
+            dataGridView1.DataSource = ds.Tables["userop"];
+            conn.Close();
         }
     }
 }
